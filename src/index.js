8ed7578c7ec01d0335c16638115a914cd88c9f5e
giveer <giveer.in@gmail.com>
2023-12-13 11:35:05 +0530

    fourth

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
-import New from './New';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/new" element={<New />} />
-      </Routes>
-    </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
