12c915a9fed51a3bb188bc96abee51db85f05fa5
giveer <giveer.in@gmail.com>
2023-12-13 11:21:00 +0530

    second commit

---
+++ src/New.js
@@ -0,0 +1,9 @@
+function New(){
+    return(
+        <div>
+            <h1>Hello Yokesh!</h1>
+        </div>
+    )
+}
+
+export default New
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
+import New from './New';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/new" element={<New />} />
+      </Routes>
+    </BrowserRouter>
 );
 
